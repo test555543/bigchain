62109b15168141fe77fe92b46b855a0c7b24c652
BigChain <test555543@gmail.com>
2025-05-08 16:17:39 +0800

    Update utils.go

--- utils.go
+++ utils.go
@@ -1,2 +1,16 @@
-// here is for utils.go
-go.md
+package main
+
+import (
+	"fmt"
+)
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	num1 := 10
+	num2 := 20
+	result := add(num1, num2)
+	fmt.Println("Result is:", result)
+}
